bdccc474a0e5287fb9f76111c6efc718932784ff
kanephan1909 <pkhang962@gmail.com>
2025-11-16 21:46:24 +0700

    Rename foodkaapp to kokoria-app

--- foodkaapp/src/utils/nativewind-setup.ts
+++
@@ -1,6 +0,0 @@
-import {Image} from 'react-native';
-import {cssInterop} from 'nativewind';
-
-// Cấu hình NativeWind để hỗ trợ className cho các component không hỗ trợ mặc định
-cssInterop(Image, {className: 'style'});
-
