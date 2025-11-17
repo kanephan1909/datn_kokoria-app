7d59565a59309237a5be356add898340c93e2233
kanephan1909 <pkhang962@gmail.com>
2025-11-17 10:34:05 +0700

    Change App Icons and Rename on IOS and Android in React Native

--- kokoria-app/android/app/src/main/java/com/kokoriaapp/MainActivity.kt
+++ kokoria-app/android/app/src/main/java/com/kokoriaapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Kokoria"
+  override fun getMainComponentName(): String = "kokoriaapp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
